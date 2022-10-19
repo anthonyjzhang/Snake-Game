25f67df7a2f3376b2f8392f43a03fe1ba9df446f
Anthony Zhang <anthony.zhang@duke.edu>
2022-10-19 03:11:13 -0400

    done?

--- src/java/Game.java
+++ src/java/Game.java
@@ -1,4 +1,17 @@
 package java;
 
-public class Game {
-}
+import javax.swing.*;
+
+public class Game extends JFrame {
+
+    public Game() {
+        this.add(new Background());
+        this.setTitle("Snake Game");
+        this.pack();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+
+}}
--- src/java/Main.java
+++ src/java/Main.java
@@ -1,4 +1,9 @@
 package java;
 
 public class Main {
+
+    public static void main(String[] args) {
+        new Game();
+    }
+
 }
